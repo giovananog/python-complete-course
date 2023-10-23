d6cb10711cb9ed4fe4a58878938cdc0c000bd309
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-10-23 09:51:10 -0300

    Create main.py

---
+++ day 1/printing/main.py
@@ -0,0 +1,19 @@
+# Write your code below this line 👇
+print("hello world!\n")
+
+print("hello world\n hello world!\n\n")
+
+print("hello"+" "+"world\n")
+
+
+
+#2
+print("\n\n----\n")
+input("what is your name?")
+print("\nHello "+input("what is your name?"))
+print("\n\n")
+
+#3
+print(len(input("What is your name? ")))
+
+

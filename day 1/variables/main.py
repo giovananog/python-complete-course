82a1379d686096d04e9c78ce689ed6a6899a5c57
Giovana Nogueira <114829638+giovananog@users.noreply.github.com>
2023-10-23 09:52:09 -0300

    Create main.py

---
+++ day 1/variables/main.py
@@ -0,0 +1,15 @@
+#qnts casas depois da virgula
+print(round(8 / 3, 3))
+print(round(8 / 3, 2))
+print(round(8 / 3, 1))
+
+#faz aproximacao direto
+print(8 // 3)
+print("\n\n")
+
+#faz a conversao de tipo direto (f-string) sem concatenação tbm
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
